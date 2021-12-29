8f496499fc75d57a73c0759de2c8002c2416705d
Mahmud Rafi <87897429+MahmudRafi@users.noreply.github.com>
2021-12-29 14:05:06 +0600

    Update start.py

--- plugins/commands/start.py
+++ plugins/commands/start.py
@@ -6,4 +6,4 @@ from config import START_MESSAGE
 
 @Client.on_message(filters.command("start"))
 async def start_message(c: Client, m: Message):    
-    await m.reply_text(f"START_MESSAGE", reply_to_message_id=m.message_id)
+    await m.reply_text(START_MESSAGE, reply_to_message_id=m.message_id)
