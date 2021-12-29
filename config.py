71f446c3993a965eb8b24e96e19b01a23e40b65e
Mahmud Rafi <87897429+MahmudRafi@users.noreply.github.com>
2021-12-29 14:03:08 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 TOKEN = os.environ.get("BOT_TOKEN")
 API_HASH = os.environ.get("API_HASH")
 API_ID = int(os.environ.get("API_ID"))
-START_MESSAGE = os.environ.get("START_MESSAGE", f"<b>@RootOfCyber</b>")
-FOOTER_TEXT = os.environ.get("FOOTER_TEXT", f"@RootOfCyber")
+START_MESSAGE = os.environ.get("START_MESSAGE", "<b>@RootOfCyber</b>")
+FOOTER_TEXT = os.environ.get("FOOTER_TEXT", "@RootOfCyber")
 TORRENTS = {}
