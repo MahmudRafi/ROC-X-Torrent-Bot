218f1e21d42c58b506b0e0547d103c23c553d760
Mahmud Rafi <87897429+MahmudRafi@users.noreply.github.com>
2021-12-29 13:38:10 +0600

    Update start.py

--- plugins/commands/start.py
+++ plugins/commands/start.py
@@ -6,5 +6,5 @@ from config import START_MESSAGE
 
 @Client.on_message(filters.command("start"))
 async def start_message(c: Client, m: Message):
-     welcomed = f"Hey <b>{message.from_user.first_name}</b>\nHi! l am ROC-X TORRENT BOT.\nI Will Help You To Find Your Items From The Torrent Sites.\nAll You Have To Do Is Just Send Me What You Want To Search For And I Will Find That For You. Don't Spam Me By Sending Continuous Messages.\n\n⚠️Pornographic contents are strictly prohibited and we will leads to ban without any warning !⚠️"
+     welcomed = f"\nHi! l am ROC-X TORRENT BOT.\nI Will Help You To Find Your Items From The Torrent Sites.\nAll You Have To Do Is Just Send Me What You Want To Search For And I Will Find That For You. Don't Spam Me By Sending Continuous Messages.\n\n⚠️Pornographic contents are strictly prohibited and we will leads to ban without any warning !⚠️"
     await m.reply_text(welcomed, START_MESSAGE, reply_to_message_id=m.message_id)
